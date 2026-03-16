0f4812a369fd42e7b8321f4a41e40057d0ac212f
Walinn Paredes <paolitomaldonadofarje@gmail.com>
2026-03-16 17:07:13 -0500

    fix: reorder imports and add "use client" directive in Spinner component

--- src/components/ui/spinner.tsx
+++ src/components/ui/spinner.tsx
@@ -1,5 +1,7 @@
-import { Loader2Icon } from "lucide-react"
+"use client"
 
+import React from "react"
+import { Loader2Icon } from "lucide-react"
 import { cn } from "@/lib/utils"
 
 function Spinner({ className, ...props }: React.ComponentProps<"svg">) {
