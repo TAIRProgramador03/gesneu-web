085738ac86b293526d024cd80520ff085e238bc0
Walinn Paredes <paolitomaldonadofarje@gmail.com>
2026-03-18 10:25:59 -0500

    fix: import React in ButtonGroup and move dayjs locale setting to convertDateAndHour function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,8 +1,6 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 import dayjs from 'dayjs';
-import 'dayjs/locale/es';
-dayjs.locale('es');
 
 
 export function cn(...inputs: ClassValue[]) {
@@ -25,6 +23,7 @@ export const convertToDateHuman = (fecha: string) => {
 
 
 export const convertDateAndHour = (fecha: string) => {
+  dayjs.locale('es');
   const fechaOriginal = fecha
   const fechaFormateada = dayjs(fechaOriginal).format('dddd D [de] MMMM [a las] HH:mm');
   return fechaFormateada.charAt(0).toUpperCase() + fechaFormateada.slice(1)
