7d4ee191bcaeef57cdfda5b4cf7e04b00d702bb9
Programador3 <programador3@tair.pe>
2026-09-10 11:51:36 -0500

    add tipo movimiento VENDIDO

--- src/components/ui/TipoMovimientoBadge.tsx
+++ src/components/ui/TipoMovimientoBadge.tsx
@@ -5,9 +5,10 @@ export const TipoMovimientoBadge = ({ tipoMovimiento = '' }: { tipoMovimiento: s
     <span className={`px-6 py-0.5 rounded-md  font-medium text-xs ${tipoMovimiento === 'BAJA' ? 'bg-red-100 text-red-700' :
       tipoMovimiento === 'ASIGNADO' ? 'bg-yellow-100 text-yellow-700' :
         tipoMovimiento === 'DISPONIBLE' ? 'bg-green-100 text-green-700' :
-          tipoMovimiento === 'TEMPORAL' ? 'bg-cyan-100 text-cyan-700' :
-            tipoMovimiento === 'REQUERIDO' ? 'bg-rose-100 text-rose-700' :
-              'bg-gray-100 text-gray-700'
+          tipoMovimiento === 'VENDIDO' ? 'bg-fuchsia-100 text-fuchsia-700' :
+            tipoMovimiento === 'TEMPORAL' ? 'bg-cyan-100 text-cyan-700' :
+              tipoMovimiento === 'REQUERIDO' ? 'bg-rose-100 text-rose-700' :
+                'bg-gray-100 text-gray-700'
       }`}>
       {tipoMovimiento
         ? tipoMovimiento.charAt(0).toUpperCase() + tipoMovimiento.slice(1).toLowerCase()
